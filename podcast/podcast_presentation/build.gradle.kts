b7c2965d1f8c5ba183b6b2c85f76bfbd68df6003
Eric Dao <eric.dao37@gmail.com>
2023-11-09 16:23:03 -0500

    Add podcast modules

---
+++ podcast/podcast_presentation/build.gradle.kts
@@ -0,0 +1,12 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+}
+apply(from = "$rootDir/compose-module.gradle")
+
+android {
+    namespace = "com.example.podcast_presentation"
+}
+dependencies {
+    implementation(Coil.coilCompose)
+}
